a42a9b90fca9f68541a0f212088c7bb360c3bb72
Konyancha1 <kmutua791@gmail.com>
2024-04-30 19:00:12 +0200

    Changes to solutions

--- src/components/layouts/navbar.jsx
+++ src/components/layouts/navbar.jsx
@@ -10,11 +10,6 @@ const Navbar = () => {
     <nav className="bg-white p-3 shadow-md font-sans">
       <div className="flex items-center justify-between mx-4 lg:mx-24">
         <img src={logo} alt="Agrisense Logo" className="h-auto w-48" />
-        <div className="hidden lg:flex items-center justify-center gap-10 flex-grow">
-          <a href="#contact" className="block lg:inline-block lg:mt-0 mr-4 font-semibold text-gray-800 hover:text-green-600">Home</a>
-          <a href="#about" className="block lg:inline-block lg:mt-0 mr-4 font-semibold text-gray-800 hover:text-green-600">About Us</a>
-          <a href="#solution" className="block lg:inline-block lg:mt-0 mr-4 font-semibold text-gray-800 hover:text-green-600">Our Solution</a>
-        </div>
         <button
             className={`flex bg-green-600 p-3 text-sm text-white font-bold rounded-lg cursor-pointer transition duration-300 hover:bg-green-700 md:px-6 md:py-3 md:text-base`}
             onClick={handleContactClick}
--- src/components/sections/solution.jsx
+++ src/components/sections/solution.jsx
@@ -9,18 +9,18 @@ const Solution = () => {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mx-auto lg:mx-2 sm:w-[65%] w-3/4 lg:w-full">
             <SolutionCard
                 icon={<FaTools />}
-                title="AI Coffee Disease"
-                description="Developing an AI-powered system for early coffee disease detection and quality monitoring. Utilizing machine learning algorithms to analyze coffee berry images, identifying disease symptoms, and quality attributes. Empowering farmers with proactive measures to protect crops and optimize yield."
+                title="Coffee Disease Detection System"
+                description="Developing an AI-powered system for early coffee disease detection. Utilizing machine learning algorithms to analyze coffee berry and coffee leaf images, identifying disease symptoms, and quality attributes. Empowering farmers with proactive measures to protect crops and optimize yield."
             />
             <SolutionCard
                 icon={<FaQuestion />}
                 title="Image Analysis System"
-                description="Implementing an image analysis system for coffee berry disease detection. Collecting images via a mobile app or web platform, analyzing with AI models trained on labeled datasets. Identifying disease indicators like color and texture abnormalities, providing real-time feedback and recommendations to farmers."
+                description="Implementing an image analysis system for coffee disease detection. Collecting images via a web platform, analyzing with AI models trained on labeled datasets. Identifying disease indicators like color and texture abnormalities, providing real-time feedback and recommendations to farmers."
             />
-            <SolutionCard
+             <SolutionCard
                 icon={<FaCheck />}
-                title="Validation & Testing"
-                description="Conducting rigorous testing and validation processes to ensure system accuracy and reliability. Utilizing diverse datasets of coffee berry images representing various disease conditions and quality levels. Comparing system predictions against ground truth labels, gathering feedback from farmers and experts for continuous improvement."
+                title="Analytics Dashboard"
+                description="Enabling coffee cooperatives and corporations to monitor farmers' crop health through our analytics dashboard. Offering customized farmer support based on real-time insights. Resulting in improved income for farmers and increased revenue and profit margins for corporations by selling high-quality coffee cherry and beans at better prices."
             />
         </div>
     </div>
