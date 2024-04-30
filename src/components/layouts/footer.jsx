ada636d8eae6b517e295a264a70a3cc6fe3e1bab
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2024-04-30 19:10:25 +0200

    Update footer.jsx

--- src/components/layouts/footer.jsx
+++ src/components/layouts/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-white p-3 border-t-2 font-sans">
       <div className="container mx-auto px-4 lg:px-24 flex justify-between items-center">
         <img src={logo} alt="Agrisense Logo" className="h-auto md:w-48 w-24" />
-        <p className="text-sm">&copy; 2024 Your Company Name. All rights reserved.</p>
+        <p className="text-sm">&copy; 2024 Agrisense. All rights reserved.</p>
       </div>
     </footer>
   );
