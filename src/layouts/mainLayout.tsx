d3e169dab37fd06d8861271c34f10b98dee68c9b
Kevin <kmutua791@gmail.com>
2024-12-18 11:14:41 +0300

    Improve footer

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -45,7 +45,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
                   />
                 </svg>
               </button>
-              <ul className="menu menu-base dropdown-content bg-white rounded-box z-[1] mt-3 w-52 p-2 shadow-lg">
+              <ul className="menu menu-sm dropdown-content bg-white rounded-box z-[1] mt-3 w-52 p-2 shadow-lg">
                 <NavLink href="#home">Home</NavLink>
                 <li>
                   <details>
